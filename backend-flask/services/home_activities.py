9daf8fdb145ba684fd7d9118f62fb02194b1b20b
brahm0 <nodowns@yahoo.com>
2023-04-22 11:23:01 +0000

    implement create activity

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,7 +5,7 @@ import logging
 from lib.db import db
 
 
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
@@ -73,20 +73,6 @@ class HomeActivities:
       ## Add custom instrumentation to Honeycomb to add more attributes.
       #span.set_attribute("app.User_id", User_id)
 
-    results = db.query_array_json("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-    """)
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
     return results
